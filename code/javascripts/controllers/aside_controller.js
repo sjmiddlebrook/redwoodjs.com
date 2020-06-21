3908bc3599a647a5b08929b0233371f3554caca5
Jack Middlebrook <jack.middlebrook@gmail.com>
2020-06-21 10:18:20 +0800

    Add github edit button and collapse outline nav

--- code/javascripts/controllers/aside_controller.js
+++ code/javascripts/controllers/aside_controller.js
@@ -3,11 +3,42 @@ import { Controller } from 'stimulus'
 let scroll
 
 export default class extends Controller {
+  static get targets() {
+    return ['pageOutline', 'outlineButton']
+  }
+
   connect() {
     this.element.scrollTop = scroll
   }
 
   saveScroll() {
     scroll = this.element.scrollTop
+  }
+
+  toggleOutline(event) {
+    event.stopPropagation()
+    event.preventDefault()
+
+    if (this.outlineButtonTarget.getAttribute('aria-expanded') === "false") {
+      this.showOutline()
+    } else {
+      this.hideOutline(event)
+    }
+  }
+
+  showOutline() {
+    this.outlineButtonTarget.setAttribute('aria-expanded', "true")
+    this.pageOutlineTarget.classList.add('block')
+    this.pageOutlineTarget.classList.remove('hidden')
+  }
+
+  hideOutline(event) {
+    if (event.target === this.pageOutlineTarget) {
+      event.preventDefault()
+      return
+    }
+    this.outlineButtonTarget.setAttribute('aria-expanded', "false")
+    this.pageOutlineTarget.classList.remove('block')
+    this.pageOutlineTarget.classList.add('hidden')
   }
 }
